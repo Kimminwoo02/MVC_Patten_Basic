f9c6e28a04949c1cb89be6f11eea975b0a1ecba4
minu <made_power@naver.com>
2023-04-02 21:53:32 +0900

    스프링 MVC 를 이용하여 구현
    앞에서 직접 만든 MVC를 스프링을 이용하여 구현해보았다.

--- src/main/java/com/example/hellosevlet/HelloSevletApplication.java
+++ src/main/java/com/example/hellosevlet/HelloSevletApplication.java
@@ -3,6 +3,10 @@ package com.example.hellosevlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceView;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan // 서블릿 자동 등록
 @SpringBootApplication
@@ -11,5 +15,6 @@ public class HelloSevletApplication {
     public static void main(String[] args) {
         SpringApplication.run(HelloSevletApplication.class, args);
     }
+
 
 }
